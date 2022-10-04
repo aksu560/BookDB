81099a7edc5f88665e6978ed1fa5ae0a543c18c4
aksu <suck@aksus.wang>
2022-10-04 19:03:40 +0300

    Deleted commented code

--- BookDB/Program.cs
+++ BookDB/Program.cs
@@ -9,9 +9,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
-// builder.Services.AddDbContext<BookDBContext>(options => options.UseInMemoryDatabase("BookDB"));
 builder.Services.AddDbContext<BookDBContext>(options => options.UseSqlite());
 
 
